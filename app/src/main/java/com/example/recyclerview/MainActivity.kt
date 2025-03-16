6da1dbef16168c1a3774437bc9d8c54d77794862
Keshav Pai <keshu.pai@gmail.com>
2025-03-16 13:34:20 +0530

    Final Version of Recycler View

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.recyclerview
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -21,6 +22,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerview.ui.theme.RecyclerViewTheme
 
 class MainActivity : AppCompatActivity() {
+    private val fruitList = listOf<Fruit>(
+        Fruit("Mango", "Joe"),
+        Fruit("Guava", "Stephen"),
+        Fruit("Lemon", "Root"),
+        Fruit("Orange", "Fedex"),
+        Fruit("Pear", "FirstCry"),
+        Fruit("Coconut", "SnapDeal"),
+        Fruit("Banana","Meesho"),
+        Fruit("Apple", "Flipkart"),
+        Fruit("SweetLime", "Amazon"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -35,7 +47,12 @@ class MainActivity : AppCompatActivity() {
         recyclerView.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_200))
 
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = MyRecyclerViewAdapter()
+        recyclerView.adapter = MyRecyclerViewAdapter(fruitList, {
+            selectedItem : Fruit -> listItemClicked(selectedItem)
+        })
+    }
 
+    private fun listItemClicked(fruit: Fruit)  {
+        Toast.makeText(this@MainActivity, "Supplier is ${fruit.supplier}", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/recyclerview/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recyclerview/MyRecyclerViewAdapter.kt
@@ -4,10 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class MyRecyclerViewAdapter : RecyclerView.Adapter<MyViewHolder>() {
-    private val fruitList = listOf<String>("Mango", "Guava", "Lemon", "Orange", "Pear", "Coconut", "Banana", "Apple", "SweetLime")
+class MyRecyclerViewAdapter(
+    private val fruitList : List<Fruit>,
+    private val clickListener : (Fruit) -> Unit
+    ) : RecyclerView.Adapter<MyViewHolder>() {
+//    private val fruitList = listOf<String>("Mango", "Guava", "Lemon", "Orange", "Pear", "Coconut", "Banana", "Apple", "SweetLime")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -22,10 +26,21 @@ class MyRecyclerViewAdapter : RecyclerView.Adapter<MyViewHolder>() {
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // This is used to display the data on the list item
         val fruit = fruitList[position]
-        holder.myTextView.text = fruit
+//        holder.myTextView.text = fruit.name + ", " + fruit.supplier
+        holder.bind(fruit, clickListener)
     }
 }
 
 class MyViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
-    val myTextView = view.findViewById<TextView>(R.id.tvName)
+    fun bind(fruit : Fruit, clickListener: (Fruit) -> Unit) {
+        val myTextView = view.findViewById<TextView>(R.id.tvName)
+        myTextView.text = fruit.name + ", " + fruit.supplier
+
+        // Adding an onClickListener
+        view.setOnClickListener {
+            clickListener(fruit)
+//            Toast.makeText(view.context, "Selected fruit is ${fruit.name}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
